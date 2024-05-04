ba132b1b2993eacd57ddd6fad7c1a3d0a176cd65
nurbq <nurbakhyt.sembayev@gmail.com>
2024-05-04 20:08:12 +0500

    change SecurityConfig

--- src/main/java/org/example/userregistr/config/security/SecurityConfig.java
+++ src/main/java/org/example/userregistr/config/security/SecurityConfig.java
@@ -15,10 +15,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,7 +24,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private CustomUserDetailsService customUserDetailsService;
+//    private CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public JwtAuthenticationConfigurer jwtAuthenticationConfigurer(
@@ -49,12 +47,13 @@ public class SecurityConfig {
         http.with(jwtAuthenticationConfigurer, Customizer.withDefaults());
         return http
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/register").permitAll()
-                        .requestMatchers("/robot").hasAuthority("ROLE_robot")
+                        .requestMatchers("/manager.html").hasRole("MANAGER")
+                        .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
-                .userDetailsService(customUserDetailsService)
+//                .userDetailsService(customUserDetailsService)
                 .build();
     }
 
