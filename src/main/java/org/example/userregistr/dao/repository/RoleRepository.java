0e4201cfb00c8b397ae9552d2cdbcd09ff810f8b
nurbq <nurbakhyt.sembayev@gmail.com>
2024-05-04 20:13:18 +0500

    add RoleRepository

--- src/main/java/org/example/userregistr/dao/repository/RoleRepository.java
+++ src/main/java/org/example/userregistr/dao/repository/RoleRepository.java
@@ -1,25 +1,32 @@
 package org.example.userregistr.dao.repository;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.example.userregistr.dao.entity.Role;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class RoleRepository {
 
     @Qualifier("primaryJdbcTemplate")
     private final JdbcTemplate jdbcTemplate;
 
-    public RoleRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    public List<String> getRolesByUserId(Long userId) {
+        String query = "select role_name " +
+                       "from roles " +
+                       "where user_id = ?";
 
+        return jdbcTemplate.queryForList(query, String.class, userId);
+    }
 
     public Long insert(Role role) {
         String query = "select nextval('roles_id_seq')";
         Long id = jdbcTemplate.queryForObject(query, Long.class);
-        query = "insert into roles (id, role_name, user_id)";
+        query = "insert into roles (id, role_name, user_id) " +
+                "values (?, ?, ?)";
 
         jdbcTemplate.update(query,
                 id,
@@ -27,5 +34,13 @@ public class RoleRepository {
                 role.getUserId());
 
         return id;
+    }
+
+    public void deleteByUserId(Long userId) {
+        String query = "delete " +
+                       "from roles " +
+                       "where user_id = ?";
+
+        jdbcTemplate.update(query, userId);
     }
 }
